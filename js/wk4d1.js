c17a5bfc05d4fa7295891bbcbb7b80804776dbb4
Salena Echols <salucky9@gmail.com>
2021-10-04 19:53:29 -0400

    wk4d1 work

---
+++ js/wk4d1.js
@@ -0,0 +1,11 @@
+
+
+function turnBlue() {
+    var elem = document.getElementById("header");
+    var styles = elem.style;
+    styles.backgroundColor = "green";
+}
+
+turnBlue('header');
+
+document.getElementById('header').style.color = "black";
